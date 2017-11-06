f13c711c71d29a65608b6e0a314e6083c22f067e
wangyu <manchesterfans@live.cn>
2017-11-07 01:23:01 +0800

    add maven support

--- src/main/java/com/wy/login_frame/controller/LoginController.java
+++ src/main/java/com/wy/login_frame/controller/LoginController.java
@@ -1,5 +1,26 @@
 package java.com.wy.login_frame.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
+@RequestMapping(value = "/")
 public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public String test(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println("hello");
+        return "hello";
+    }
 }
--- src/main/java/com/wy/login_frame/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wy/login_frame/exception/GlobalExceptionHandler.java
@@ -1,13 +1,21 @@
 package java.com.wy.login_frame.exception;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(value = Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest request, Exception e) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("exception", e);
+        modelAndView.addObject("exception", e.getMessage().toString());
         modelAndView.addObject("url", request.getRequestURL());
         modelAndView.setViewName("error");
+        e.printStackTrace();
         return modelAndView;
     }
 }
