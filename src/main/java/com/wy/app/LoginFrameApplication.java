a325dd066512ce3e0585a57908bddb92cfddbf69
wangyu <manchesterfans@live.cn>
2017-11-14 01:09:58 +0800

    add redis to share sesssions

--- src/main/java/com/wy/app/LoginFrameApplication.java
+++ src/main/java/com/wy/app/LoginFrameApplication.java
@@ -2,10 +2,12 @@ package com.wy.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableRedisHttpSession
 public class LoginFrameApplication {
 
 	public static void main(String[] args) {
