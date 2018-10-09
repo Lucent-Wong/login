80a6633768e02c7edab51c552038be9d59bb741b
wangyu <manchesterfans@live.cn>
2018-10-09 21:39:04 +0800

    refector entity

--- common/src/main/java/com/primedice/common/entity/Role.java
+++ common/src/main/java/com/primedice/common/entity/Role.java
@@ -1,96 +1,24 @@
 package com.primedice.common.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Role implements Serializable {
     private Long id;
-    private String role; //角色标识 程序中判断使用,如"admin"
-    private String description; //角色描述,UI界面显示使用
-    private Boolean available = Boolean.FALSE; //是否可用,如果不可用将不会添加给用户
+    private String role;
+    private String description;
+    private Boolean available;
     private Integer serviceTotal;
     private Integer serviceCallTotal;
 
-    public Role() {
-    }
-
-    public Role(String role, String description, Boolean available) {
-        this.role = role;
-        this.description = description;
-        this.available = available;
-    }
-
-    public void setServiceCallTotal(Integer serviceCallTotal) {
-        this.serviceCallTotal = serviceCallTotal;
-    }
-
-    public Integer getServiceCallTotal() {
-        return serviceCallTotal;
-    }
-
-    public void setServiceTotal(Integer serviceTotal) {
-        this.serviceTotal = serviceTotal;
-    }
-
-    public Integer getServiceTotal() {
-        return serviceTotal;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Role role = (Role) o;
-
-        if (id != null ? !id.equals(role.id) : role.id != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", role='" + role + '\'' +
-                ", description='" + description + '\'' +
-                ", available=" + available +
-                '}';
-    }
 }
--- common/src/main/java/com/primedice/common/entity/RolePermission.java
+++ common/src/main/java/com/primedice/common/entity/RolePermission.java
@@ -1,53 +1,21 @@
 package com.primedice.common.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RolePermission implements Serializable {
 
     private Long roleId;
     private Long permissionId;
 
-    public Long getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Long roleId) {
-        this.roleId = roleId;
-    }
-
-    public Long getPermissionId() {
-        return permissionId;
-    }
-
-    public void setPermissionId(Long permissionId) {
-        this.permissionId = permissionId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        RolePermission that = (RolePermission) o;
-
-        if (permissionId != null ? !permissionId.equals(that.permissionId) : that.permissionId != null) return false;
-        if (roleId != null ? !roleId.equals(that.roleId) : that.roleId != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = roleId != null ? roleId.hashCode() : 0;
-        result = 31 * result + (permissionId != null ? permissionId.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "RolePermssion{" +
-                "roleId=" + roleId +
-                ", permissionId=" + permissionId +
-                '}';
-    }
 }
